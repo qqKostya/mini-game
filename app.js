d9bdc8f05af675476b61472aba710f9453be516d
Kostya <leap74rus@gmail.com>
2022-10-16 23:20:59 +0300

    upd js code

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const board = document.querySelector('#board');
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const colors = ['#FF69B4', '#FF4500', '#8A2BE2', '#FFD700', '#000080']
 const SQUARES_NUMBER = 500;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
+    const square = document.createElement('div');
+    square.classList.add('square');
 
-  square.addEventListener('mouseover', () => {
-    setColor(square)
-  })
+    square.addEventListener('mouseover', setColor)
 
-  square.addEventListener('mouseleave', () => {
-    removeColor(square)
-  })
+    square.addEventListener('mouseleave', removeColor)
 
-  board.append(square);
+    board.append(square);
 }
 
-function setColor(element) {
-  const color = getRandomColor();
-  element.style.backgroundColor = color;
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+function setColor(event) {
+    const element = event.target;
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(element) {
-  element.style.backgroundColor = '#1D1D1D';
-  element.style.boxShadow = `0 0 2px #000`;
+function removeColor(event) {
+    const element = event.target;
+    element.style.backgroundColor = '#1D1D1D';
+    element.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
